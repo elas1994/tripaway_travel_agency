fadac0f7f343979b8c4626261738575de0231f50
Sale <s_skoric@hotmail.com>
2023-12-04 18:54:06 +0100

    conecting router , adding pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link, Outlet } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Link to="/tripaway_travel_agency/">Home</Link>
+        {" | "}
+        <Link to="/tripaway_travel_agency/about">About</Link>
+        {" | "}
+        <Link to="/tripaway_travel_agency/service">Service</Link>
+        {" | "}
+        <Link to="/tripaway_travel_agency/contact">Contact</Link>
+        {" | "}
+        <Link to="/tripaway_travel_agency/gallery">Gallery</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Service from "./pages/Service.jsx";
+import Contact from "./pages/Contact.jsx";
+import Gallery from "./pages/Gallery.jsx";
+
+const router123 = createBrowserRouter([
+  {
+    path: "/tripaway_travel_agency/",
+    element: <App />,
+    children: [
+      {
+        path: "/tripaway_travel_agency/",
+        element: <Home />,
+      },
+      {
+        path: "/tripaway_travel_agency/about",
+        element: <About />,
+      },
+      {
+        path: "/tripaway_travel_agency/service",
+        element: <Service />,
+      },
+      {
+        path: "/tripaway_travel_agency/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/tripaway_travel_agency/gallery",
+        element: <Gallery />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router123} />
+  </React.StrictMode>
+);
